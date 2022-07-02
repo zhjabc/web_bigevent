9039e8b7072c8b98b5e2a8d9c20f07c5346879ce
zhjabc <1026607377@qq.com>
2022-07-02 19:09:20 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,16 @@
 $.ajaxPrefilter(function (options) {
   // console.log(options);
   options.url = 'http://www.liulongbin.top:3007' + options.url;
+
+  //统一设置headers
+  if (options.url.indexOf('/my/') != -1) {
+    options.headers = { Authorization: localStorage.getItem('token') || '' };
+  }
+  //统一挂载complete回调函数
+  options.complete = function (res) {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      localStorage.removeItem('token');
+      location.href = '/login.html';
+    }
+  };
 });
